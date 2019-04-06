aa76e15b606cae3e5aaf4758a6a607f27aaa4fc6
alby_winsum <albyally5@gmail.com>
2019-04-06 12:57:34 +0300

    Edit to app.js file with Mr. Andela swag

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
  //This is a IIFE(Immeadiately Invokable Function Expression).
 
   (function (){
-    console.log('Hello! My name is alby,);
+    console.log('Hello! My name is alby, and I'm Mr. Andela);
    });
